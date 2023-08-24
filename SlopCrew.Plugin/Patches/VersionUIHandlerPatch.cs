0204090c89b6b57ed73a3b52de741b7b2ffd207c
funcjay <funcjay@gmail.com>
2023-08-24 18:03:57 -0400

    slop in the menu

---
+++ SlopCrew.Plugin/Patches/VersionUIHandlerPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using Reptile;
+using TMPro;
+
+namespace SlopCrew.Plugin.Patches;
+
+[HarmonyPatch(typeof(VersionUIHandler))]
+public class VersionUIHandlerPatch {
+    [HarmonyPostfix]
+    [HarmonyPatch("SetVersionText")]
+    private static void SetVersionText(VersionUIHandler __instance) {
+        var obj = __instance.versionText;
+        var verText = obj.text;
+        
+        obj.alignment = TextAlignmentOptions.BottomLeft;
+        obj.text = $"<color=\"purple\">SlopCrew v{PluginInfo.PLUGIN_VERSION}\n" + verText;
+    }
+}
