38982385754c94f37d30bf254c894c782566a9da
r00tz <razor950@gmail.com>
2023-08-27 00:49:16 -0400

    Various Changes
    Cleaned up ServerConnection.cs
    Optimized BroadcastNewPlayers function
    Added Errormine's changes needed for position improvements

---
+++ SlopCrew.Common/Network/Clientbound/ClientBoundSync.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using static SlopCrew.Common.Network.NetworkMessageType;
+
+namespace SlopCrew.Common.Network.Clientbound; 
+
+public class ClientBoundSync : NetworkPacket {
+    public override NetworkMessageType MessageType => ClientboundSync;
+    
+    public uint ServerTickActual;
+
+    public override void Read(BinaryReader br) {
+        this.ServerTickActual = br.ReadUInt32();
+    }
+
+    public override void Write(BinaryWriter bw) {
+        bw.Write(this.ServerTickActual);
+    }
+}
