233406d398820fcbcad99341c38888b76adb44dd
NotNite <hi@notnite.com>
2023-08-29 17:21:37 -0400

    Only show connection status when phone is open

--- SlopCrew.Plugin/UI/UIConnectionStatus.cs
+++ SlopCrew.Plugin/UI/UIConnectionStatus.cs
@@ -1,17 +1,19 @@
-﻿using HarmonyLib;
+﻿using System;
+using HarmonyLib;
 using Reptile;
+using Reptile.Phone;
 using TMPro;
 using UnityEngine;
 
-namespace SlopCrew.Plugin.UI; 
+namespace SlopCrew.Plugin.UI;
 
 public class UIConnectionStatus : MonoBehaviour {
     private TextMeshProUGUI tmp = null!;
-    
+
     private void Awake() {
         var obj = new GameObject("SlopCrew_UIConnectionStatus");
         tmp = obj.AddComponent<TextMeshProUGUI>();
-        
+
         // We love stealing shit
         var uiManager = Core.Instance.UIManager;
         var gameplay = Traverse.Create(uiManager).Field<GameplayUI>("gameplay").Value;
@@ -24,13 +26,25 @@ public class UIConnectionStatus : MonoBehaviour {
         // epic positioning
         this.tmp.alignment = TextAlignmentOptions.TopLeft;
         var rect = this.tmp.rectTransform;
+
         // this goes top left, because x0 y0 is bottom right in Unity :^) - jay
         rect.anchorMin = new Vector2(0, 0.5f);
         rect.anchorMax = new Vector2(0.5f, 1);
         rect.pivot = new Vector2(0, 1);
         rect.anchoredPosition = new Vector2(0.2f, 0.9f);
-        
+
         this.tmp.rectTransform.SetParent(gameplay.gameplayScreen.GetComponent<RectTransform>(), false);
+    }
+
+    private void Update() {
+        var player = WorldHandler.instance.GetCurrentPlayer();
+        if (player is null || !player.isActiveAndEnabled) {
+            this.tmp.enabled = false;
+            return;
+        }
+
+        var phone = Traverse.Create(player).Field<Phone>("phone").Value;
+        this.tmp.enabled = phone.transform.Find("OpenCanvas").gameObject.activeSelf;
     }
 
     private void FixedUpdate() {
