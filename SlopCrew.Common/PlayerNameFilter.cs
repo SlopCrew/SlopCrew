a166c488db445f41c26df7093c056fcc9a926b1f
funcjay <funcjay@gmail.com>
2023-08-29 18:13:06 +0200

    Move profanity filter and add exceptions

---
+++ SlopCrew.Common/PlayerNameFilter.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace SlopCrew.Common; 
+
+public static class PlayerNameFilter {
+    private static ProfanityFilter.ProfanityFilter Filter = new();
+    
+    // these names are caught in the filter. exclude them.
+    private static List<String> BasedNames = new() {
+        "onlinegangbanger",
+        "<color=#00ff37>DICK <color=#f4fff2>GRIPPA"
+    };
+    
+    public static string DoFilter(string name) {
+        if (BasedNames.Contains(name)) {
+            return name;
+        }
+        
+        if (Filter.ContainsProfanity(name.ToLower())) {
+            return "Punished Slopper";
+        }
+
+        var len = Math.Min(32, name.Length);
+        return name.Substring(0, len);
+    }
+}
