7ad65b38fb0e98d6868e05409dd37a12243cce67
NotNite <hi@notnite.com>
2023-08-30 17:45:06 -0400

    my ass left debug logs in

--- SlopCrew.Server/SlopAPIController.cs
+++ SlopCrew.Server/SlopAPIController.cs
@@ -10,7 +10,6 @@ namespace SlopCrew.Server;
 public class SlopAPIController : WebApiController {
     [Route(HttpVerbs.Get, "/metrics")]
     public async Task GetMetrics() {
-        Log.Information("test");
         var metrics = Server.Instance.Metrics;
         var response = new {
             connections = metrics.Connections,
@@ -18,7 +17,6 @@ public class SlopAPIController : WebApiController {
         };
 
         var json = JsonSerializer.Serialize(response);
-        Console.WriteLine("json " + json);
         await this.HttpContext.SendStringAsync(json, "application/json", Encoding.UTF8);
     }
 }
