af6cbcf349bfb33206085d8e526ae5e0192d5400
NotNite <hi@notnite.com>
2023-09-03 17:24:03 -0400

    Hash each file in downloaded publicized zip

--- gha.ps1
+++ gha.ps1
@@ -3,4 +3,8 @@
 New-Item -ItemType Directory -Path ./local
 Invoke-WebRequest -Uri https://sloppers.club/publicized.zip -OutFile ./local/BombRushCyberfunk.zip
 Expand-Archive -Path ./local/BombRushCyberfunk.zip -DestinationPath ./local/BombRushCyberfunk
+
+Get-FileHash ./local/BombRushCyberfunk.zip
+Get-FileHash -Path "./local/BombRushCyberfunk/Bomb Rush Cyberfunk_Data/Managed/*.*"
+
 Remove-Item ./local/BombRushCyberfunk.zip
