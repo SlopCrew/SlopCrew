87259051c15f89d4d9425d1c25f0d3b156cf434f
NotNite <hi@notnite.com>
2023-09-03 15:35:05 -0400

    Fix zip-in-zip

--- gha.ps1
+++ gha.ps1
@@ -1,3 +1,5 @@
+# https://github.com/BepInEx/BepInEx.AssemblyPublicizer
+# assembly-publicizer "./local/BombRushCyberfunk/Bomb Rush Cyberfunk_Data/Managed/" --overwrite --strip-only
 New-Item -ItemType Directory -Path ./local
 Invoke-WebRequest -Uri https://sloppers.club/publicized.zip -OutFile ./local/BombRushCyberfunk.zip
 Expand-Archive -Path ./local/BombRushCyberfunk.zip -DestinationPath ./local/BombRushCyberfunk
