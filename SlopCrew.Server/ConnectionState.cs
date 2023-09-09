0369ad2da8c7f4d0f56ceb09686445108402892d
Michael <15317421+ActualMandM@users.noreply.github.com>
2023-09-09 16:04:32 -0700

    Separate invalid character check into 3 separate checks
    Fixes CharacterAPI players being forced to on foot even when they're not

--- SlopCrew.Server/ConnectionState.cs
+++ SlopCrew.Server/ConnectionState.cs
@@ -94,14 +94,14 @@ public class ConnectionState {
 
     private void HandleHello(ServerboundPlayerHello enter, Server server) {
         // Temporary solution to CharacterAPI players crashing other players
-        var isInvalid = enter.Player.Character is < -1 or > 26
-                        || enter.Player.Outfit is < 0 or > 3
-                        || enter.Player.MoveStyle is < 0 or > 5;
-        if (isInvalid) {
-            enter.Player.Character = 3; // Red
+        if (enter.Player.Character is < 0 or > 25)
+            enter.Player.Character = 3;
+
+        if (enter.Player.Character is < 0 or > 3)
             enter.Player.Outfit = 0;
+
+        if (enter.Player.MoveStyle is < 0 or > 4)
             enter.Player.MoveStyle = 0;
-        }
 
         // Assign a unique ID on first hello
         // Subsequent hellos keep the originally assigned ID
