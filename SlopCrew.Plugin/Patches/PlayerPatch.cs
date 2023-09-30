c1606607fa63466eb0a61658a3fe7af135caf5e1
viliger <wowlocale@gmail.com>
2023-09-30 22:31:10 +0300

    Fixed an issue with player having incorrect outfit after changing character via cypher.

--- SlopCrew.Plugin/Patches/PlayerPatch.cs
+++ SlopCrew.Plugin/Patches/PlayerPatch.cs
@@ -67,6 +67,12 @@ public class PlayerPatch {
     [HarmonyPatch("SetCharacter")]
     public static void SetCharacter(Player __instance, Characters setChar, int setOutfit = 0) {
         if (__instance == WorldHandler.instance?.GetCurrentPlayer()) {
+            //since the game sets outfit after calling SetCharacter in CharacterSelect.SetPlayerToCharacter
+            //via setting outfit material manually instead of just using outfit parameter, we have to also set it manually
+            var outfit = Reptile.Core.Instance.SaveManager.CurrentSaveSlot.GetCharacterProgress(setChar)?.outfit;
+            if (outfit != null) {
+                Plugin.PlayerManager.CurrentOutfit = (int) outfit;
+            }
             Plugin.PlayerManager.IsHelloRefreshQueued = true;
         }
     }
